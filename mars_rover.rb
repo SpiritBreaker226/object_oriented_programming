44b21990b60a9fa09cf8e72fcdbfce23a7ba686a
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-22 17:06:46 -0500

    Add read instruction method and add methods move and turn in order to test also removes the mars_rover as it the rename version of rover.rb

--- mars_rover.rb
+++
@@ -1,9 +0,0 @@
-class Rover
-	attr_accessor :x, :y, :direction
-
-	def initialize(x, y, direction)
-		@x = x
-		@y = y
-		@direction = direction
-	end
-end
--- rover.rb
+++ rover.rb
@@ -6,4 +6,38 @@ class Rover
 		@y = y
 		@direction = direction
 	end
+
+	def read_instruction(instructions)
+		instructions.each_char do |instruction| 
+			if instruction == "M"
+				move
+			elsif
+				turn(instruction)
+			end
+		end
+	end
+
+	private
+
+	def move
+	end
+
+	def turn(direction)
+	end
 end
+
+rover1 = Rover.new(1, 2, "N")
+rover1.read_instruction("LML")
+
+# 5.times do |input|
+# 	puts "Enter a input"
+# 	input = gets.chomp
+
+# 	instruction = input.split(" ")
+
+# 	if instruction.count == 1
+
+# 	end
+	
+# end
+
