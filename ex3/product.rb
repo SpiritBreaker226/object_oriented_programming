490fbbb1140f937ad8776452e4d83e6b7b56208b
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-25 19:29:26 -0500

    Change initialize to use hash in order to have a more meanfully agruments

--- ex3/product.rb
+++ ex3/product.rb
@@ -1,10 +1,10 @@
 class Product
 	attr_reader :name, :total
 
-	def initialize(qty, name, price)
-		@qty = qty.to_i
-		@name = name.to_s
-		@total = @qty * price.to_f
+	def initialize(optional = {})
+		@qty   = optional[:qty].to_i || 0
+		@name  = optional[:name].to_s || 0
+		@total = (@qty * optional[:price].to_f) || 0.00
 	end
 
 	def add_tax(tax_rate)
