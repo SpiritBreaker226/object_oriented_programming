78dae5aee783f7928dafe3a8ee67c1d9b25f0d89
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-01 17:44:36 -0500

    Create Add & Subtracting Methods To Point Class
    Create both an Add method to add either point and a subtracting method
    to subtract either point, in the Point class because those point change
    as in the application.

--- point.rb
+++ point.rb
@@ -5,6 +5,14 @@ class Point
 		@x = x
 		@y = y
 	end
+
+	def add_x_y(which_point_to_add, amount_add_to)
+		which_point_to_add == :x ? @x += amount_add_to : @y += amount_add_to
+	end
+
+	def subtract_x_y(which_point_to_subtract, amount_subtract_to)
+		which_point_to_subtract == :x ? @x -= amount_subtract_to : @y -= amount_subtract_to
+	end
 	
 	def to_s
 		"#{x} #{y}"
